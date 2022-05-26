c1ae5f55c4a314a75c484f09365f062d9396026e
elina jaiani <elinajaiani@gmail.com>
2022-05-26 22:31:17 +0400

    array

---
+++ jshomerk.js
@@ -0,0 +1,22 @@
+var (numbers1) = (0,1,2,3,4,5);
+
+var (numbers2) = [[0,1,2],[3,4,5]]
+
+var myArray = new Array;
+
+
+var people =[
+    ["Tom", 25, false],
+    ["Bill", 38, true]
+    ["Alice", 23, false]
+];
+
+console.log(people[0][2]);
+
+console.log(people[1][1]);
+
+console.log(people[2][2]);
+
+// false, 38, false
+
+
