2a7fad76136478373ff43da79ccb8eb119dc4841
elina jaiani <elinajaiani@gmail.com>
2022-05-27 00:53:33 +0400

    price function

--- jshomerk.js
+++ jshomerk.js
@@ -1,6 +1,6 @@
-var (numbers1) = (0,1,2,3,4,5);
+var numbers1 = (0,1,2,3,4,5);
 
-var (numbers2) = [[0,1,2],[3,4,5]]
+var numbers2 = [[0,1,2],[3,4,5]]
 
 var myArray = new Array;
 
@@ -18,5 +18,41 @@ console.log(people[1][1]);
 console.log(people[2][2]);
 
 // false, 38, false
+
+console.log(people[0][0] + "  " + people[1][0] + "  " + people[2][0]);
+
+//Tom  Bill  Alice
+
+
+var income = prompt ("შეიყვანეთ თქვენი მიმდინარე შემოსავალი");
+if (income<400){
+    var message="თქვენი ანაზღაურება საშუალოზე დაბალია";
+    alert(message)
+}
+
+else if (income >=400 && income <=800){
+    alert("თქვენ გაქვთ საშუალო ანაზღაურება");
+}
+
+else{
+    alert("თქვენი ანაზღაურება საშუალოზე მაღალია");
+}
+
+// დავალება 2
+
+function price (x,testmessage) {
+    var message = testmessage(x);
+    document.write(message);
+}
+function informationaboutprice(price){
+    if (price<20)
+    return "ძალიან იაფია";
+    else if (price<50)
+    return "საშუალო ფასია";
+    else
+    return "ძალიან ძვირია";
+}
+price(45,informationaboutprice);
+
 
 
